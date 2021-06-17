a3df01f8742a39744f182f8190256631145a60de
Hafise Alyanga Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2021-06-17 15:25:24 +0300

    Sample routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,33 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+  
+//Route that sends back a view
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Route to users - string
+Route::get('/users', function (){
+    return 'welcome to the users page';
+});
+
+//Route to users - array(JSON)
+Route::get('/users', function (){
+    return ['PHP', 'HTML', 'Laravel'];
+});
+
+
+//Route to users - JSON Object
+Route::get('/users', function(){
+    return response()->json([
+        'name'=>'Patrick',
+        'course'=>'Begineer Laravel'
+    ]);
+});
+
+
+//Routes to users - Function
+Route::get('/users', function(){
+    return redirect('/');
 });
